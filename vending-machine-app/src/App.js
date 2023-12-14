ca2568d0fc026230021a88b499d88121621531c2
Zdenek Andrew Bocek <zabocek@gmail.com>
2023-12-14 17:49:31 -0500

    put my snack folder in my src folder and transferred App.js and App.css into my src folder

--- vending-machine-app/src/App.js
+++ vending-machine-app/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import VendingMachine from './VendingMachine';
+import Chips from './Chips';
+import Soda from './Soda';
+import CandyBar from './Candy'; // Ensure this is imported with the correct name
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={VendingMachine} />
+        <Route path="/snack/chips" component={Chips} />
+        <Route path="/snack/soda" component={Soda} />
+        <Route path="/snack/candy" component={CandyBar} />
+      </Switch>
+    </Router>
   );
 }
 
